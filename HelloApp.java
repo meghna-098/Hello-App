c0ea12c78a9768ae7227f8831670a4a583f346d5
meghna-098 <mv7778@srmist.edu.in>
2026-04-17 22:21:41 +0530

    Coded UC1: Display Hello World in console

---
+++ HelloApp.java
@@ -0,0 +1,17 @@
+/**
+ * UC1: Display "Hello World"
+ * A simple Java program to print Hello World to console
+ */
+public class HelloApp {
+
+    /**
+     * Main method - Entry point of the application
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print message to console
+        System.out.println("Hello World");
+    }
+}
